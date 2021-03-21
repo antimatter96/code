b3773be23dfa0f58085ca73377607e5655ddd010
Arpit Jain <antimatter96@users.noreply.github.com>
2021-03-22 00:52:44 +0530

    count-items-matching-a-rule

---
+++ leetcode_2/count-items-matching-a-rule.go
@@ -0,0 +1,14 @@
+package main
+
+func countMatches(items [][]string, ruleKey string, ruleValue string) int {
+	mp := map[string]int{"type": 0, "color": 1, "name": 2}
+
+	count := 0
+
+	for _, item := range items {
+		if item[mp[ruleKey]] == ruleValue {
+			count++
+		}
+	}
+	return count
+}
