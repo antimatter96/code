5540ec494908d483c0c6840e853282b6c6b851ee
Arpit Jain <antimatter96@users.noreply.github.com>
2021-03-22 00:38:47 +0530

    richest-customer-wealth

---
+++ leetcode_2/richest-customer-wealth.go
@@ -0,0 +1,16 @@
+package main
+
+func maximumWealth(accounts [][]int) int {
+	max := 0
+	for _, balances := range accounts {
+		sum := 0
+		for _, balance := range balances {
+			sum += balance
+		}
+
+		if max < sum {
+			max = sum
+		}
+	}
+	return max
+}
