f1d68ea787bee50fe4ff11ef52dfc5e294ff1823
Arpit Jain <antimatter96@users.noreply.github.com>
2021-03-22 00:37:42 +0530

    jewels-and-stones

---
+++ leetcode_2/jewels-and-stones.go
@@ -0,0 +1,18 @@
+package main
+
+func numJewelsInStones(jewels string, stones string) int {
+	mp := make(map[rune]bool)
+
+	for _, c := range jewels {
+		mp[c] = true
+	}
+
+	count := 0
+	for _, c := range stones {
+		if mp[c] {
+			count++
+		}
+	}
+
+	return count
+}
