24355d9c411703f516022d92eee1000a5287b26a
Arpit Jain <antimatter96@users.noreply.github.com>
2021-03-22 00:37:17 +0530

    kids-with-the-greatest-number-of-candies

---
+++ leetcode_2/kids-with-the-greatest-number-of-candies.go
@@ -0,0 +1,18 @@
+package main
+
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+	max := 0
+	for _, e := range candies {
+		if max < e {
+			max = e
+		}
+	}
+	arr := make([]bool, len(candies))
+	for i, e := range candies {
+		if e+extraCandies >= max {
+			arr[i] = true
+		}
+	}
+	return arr
+
+}
