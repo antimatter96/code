08d8e553468d198a83163ec06155451eeace3e04
Arpit Jain <antimatter96@users.noreply.github.com>
2021-03-22 00:39:04 +0530

    number-of-good-pairs

---
+++ leetcode_2/number-of-good-pairs.go
@@ -0,0 +1,15 @@
+package main
+
+func numIdenticalPairs(nums []int) int {
+	mp := make(map[int]int)
+	for _, e := range nums {
+		mp[e]++
+	}
+
+	count := 0
+	for _, v := range mp {
+		count += (v) * (v - 1)
+	}
+
+	return count / 2
+}
