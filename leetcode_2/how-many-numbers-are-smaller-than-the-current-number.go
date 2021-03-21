2a6ed04faddd61472c0104311bd4deeac97edbc4
Arpit Jain <antimatter96@users.noreply.github.com>
2021-03-22 00:37:30 +0530

    how-many-numbers-are-smaller-than-the-current-number

---
+++ leetcode_2/how-many-numbers-are-smaller-than-the-current-number.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"sort"
+)
+
+func smallerNumbersThanCurrent(nums []int) []int {
+	unsorted := make([]int, len(nums))
+	copy(unsorted, nums)
+	sort.Ints(nums)
+
+	mp := make(map[int]int)
+	mp[nums[0]] = 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			mp[nums[i]] = i
+		}
+	}
+
+	for i := 0; i < len(nums); i++ {
+		unsorted[i] = mp[unsorted[i]]
+
+	}
+
+	return unsorted
+}
