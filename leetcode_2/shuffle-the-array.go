a93a198e9aa68ae7fd5cf691c49e8fa2740159a7
Arpit Jain <antimatter96@users.noreply.github.com>
2021-03-22 00:37:00 +0530

    shuffle-the-array

---
+++ leetcode_2/shuffle-the-array.go
@@ -0,0 +1,12 @@
+package main
+
+func shuffle(nums []int, n int) []int {
+	arr := make([]int, 2*n)
+	for i := 0; i < n; i++ {
+		arr[2*i] = nums[i]
+	}
+	for i := 0; i < n; i++ {
+		arr[(2*i)+1] = nums[i+n]
+	}
+	return arr
+}
