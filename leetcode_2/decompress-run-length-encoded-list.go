8913400855a22e14efbbaeecaaf30279a49d2f86
Arpit Jain <antimatter96@users.noreply.github.com>
2021-03-22 02:16:54 +0530

    decompress-run-length-encoded-list

---
+++ leetcode_2/decompress-run-length-encoded-list.go
@@ -0,0 +1,17 @@
+package main
+
+func decompressRLElist(nums []int) []int {
+	total := 0
+	for i := 0; i < len(nums); i += 2 {
+		total += nums[i]
+	}
+
+	arr := make([]int, 0, total)
+	for i := 0; i < len(nums); i += 2 {
+		for j := 0; j < nums[i]; j++ {
+			arr = append(arr, nums[i+1])
+		}
+	}
+
+	return arr
+}
