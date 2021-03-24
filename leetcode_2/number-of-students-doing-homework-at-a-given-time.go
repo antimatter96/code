937bf74cbdcea857d17ebd5e6e8c243a35f49f83
Arpit Jain <me.arpitjain09@gmail.com>
2021-03-25 05:18:33 +0530

    number-of-students-doing-homework-at-a-given-time

--- leetcode_2/number-of-students-doing-homework-at-a-given-time.go
+++ leetcode_2/number-of-students-doing-homework-at-a-given-time.go
@@ -3,7 +3,9 @@ package main
 func busyStudent(startTime []int, endTime []int, queryTime int) int {
 	count := 0
 
-	for i := 0; i < len(startTime); i++ {
+	n := len(startTime)
+
+	for i := 0; i < n; i++ {
 
 		if queryTime >= startTime[i] && queryTime <= endTime[i] {
 			count++
