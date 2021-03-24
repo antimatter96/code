44d9730cb8871451027beb7b6ecf0c351edb7a7d
Arpit Jain <me.arpitjain09@gmail.com>
2021-03-25 04:47:42 +0530

    add main driver

---
+++ leetcode_2/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	// nums := []int{0, 1, 2, 3, 4}
+	// index := []int{0, 1, 2, 2, 1}
+
+	// fmt.Println(createTargetArray(nums, index))
+
+	// obj := Constructor(5)
+	// fmt.Println(obj.Insert(3, "c"))
+	// fmt.Println(obj.Insert(1, "a"))
+	// fmt.Println(obj.Insert(2, "b"))
+	// fmt.Println(obj.Insert(5, "e"))
+	// fmt.Println(obj.Insert(4, "d"))
+
+	// fmt.Println(sumOddLengthSubarrays([]int{1, 4, 2, 5, 3}))
+	// fmt.Println(sumOddLengthSubarrays([]int{1, 2}))
+	// fmt.Println(sumOddLengthSubarrays([]int{10, 11, 12}))
+
+	// fmt.Println(sumOddLengthSubarrays2([]int{1, 4, 2, 5, 3}))
+	// fmt.Println(sumOddLengthSubarrays2([]int{1, 2}))
+	// fmt.Println(sumOddLengthSubarrays2([]int{10, 11, 12}))
+
+	// fmt.Println(countGoodTriplets([]int{3, 0, 1, 1, 9, 7}, 7, 2, 3))
+	// fmt.Println(countGoodTriplets([]int{1, 1, 2, 2, 3}, 0, 0, 1))
+	// fmt.Println(countGoodTriplets([]int{7, 3, 7, 3, 12, 1, 12, 2, 3}, 5, 8, 1))
+
+	// minTimeToVisitAllPoints([][]int{[]int{1, 1}, []int{3, 4}, []int{-1, 0}})
+	// minTimeToVisitAllPoints([][]int{[]int{3, 2}, []int{-2, 2}})
+
+	// fmt.Println(findNumbers([]int{12, 345, 2, 6, 7896}))
+	// fmt.Println(findNumbers([]int{555, 901, 482, 1771, 0, 1, 10, 100}))
+
+	// fmt.Println(oddCells(2, 3, [][]int{[]int{0, 1}, []int{1, 1}}))
+	// fmt.Println(oddCells(2, 2, [][]int{[]int{1, 1}, []int{0, 0}}))
+
+	// fmt.Println(flipAndInvertImage([][]int{{1, 1, 0}, {1, 0, 1}, {0, 0, 0}}))
+
+	// fmt.Println(flipAndInvertImage([][]int{{1, 1, 0, 0}, {1, 0, 0, 1}, {0, 1, 1, 1}, {1, 0, 1, 0}}))
+
+	fmt.Println(diagonalSum([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}))
+	fmt.Println(diagonalSum([][]int{{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}}))
+	fmt.Println(diagonalSum([][]int{{5}}))
+}
