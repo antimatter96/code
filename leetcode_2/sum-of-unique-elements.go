53d21fb6dd69cfbd0b816fb02625fa830ae7e926
Arpit Jain <me.arpitjain09@gmail.com>
2021-03-28 21:07:21 +0530

    sum-of-unique-elements

---
+++ leetcode_2/sum-of-unique-elements.go
@@ -0,0 +1,19 @@
+package main
+
+func sumOfUnique(nums []int) int {
+	mp := make(map[int]int)
+
+	for _, i := range nums {
+		mp[i]++
+	}
+
+	sum := 0
+
+	for k, v := range mp {
+		if v == 1 {
+			sum += k
+		}
+	}
+
+	return sum
+}
