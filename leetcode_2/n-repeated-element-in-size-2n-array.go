862d4a7d8aa63e8d6c69b703a4e1039dcd292a94
Arpit Jain <me.arpitjain09@gmail.com>
2021-03-28 21:06:56 +0530

    n-repeated-element-in-size-2n-array

---
+++ leetcode_2/n-repeated-element-in-size-2n-array.go
@@ -0,0 +1,15 @@
+package main
+
+func repeatedNTimes(A []int) int {
+	mp := make(map[int]bool)
+
+	for _, v := range A {
+		if mp[v] {
+			return v
+		} else {
+			mp[v] = true
+		}
+	}
+
+	return -1
+}
